697b266de7ec100dc3e98a40dd6ebc78707fac6f
miteshkumar77 <miteshkumarca@gmail.com>
2020-05-23 22:45:37 -0400

    dep

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter'; 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
