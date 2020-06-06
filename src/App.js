77104423df3efa31907d107ed080586f8cf372c9
miteshkumar77 <miteshkumarca@gmail.com>
2020-06-05 21:33:10 -0400

    added resume button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './components/navbar';
-import About from './components/about'; 
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline'; 
-import { Container, MuiThemeProvider } from '@material-ui/core';
-import Theme from './theme'; 
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./components/navbar";
+import About from "./components/about";
+import Resume from "./components/resume";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { Container, MuiThemeProvider } from "@material-ui/core";
+import Theme from "./theme";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  }
-}))
+  },
+}));
 function App() {
-  const classes = useStyles(); 
+  const classes = useStyles();
   return (
     <div className={classes.root}>
       <MuiThemeProvider theme={Theme}>
-        <CssBaseline/>
-        <Navbar/> 
+        <CssBaseline />
+        <Navbar />
         <Container fixed>
-          <About/>
-          <hr className='solid'/>
+          <About />
+          <hr className="solid" />
         </Container>
       </MuiThemeProvider>
     </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react'; 
-import ReactDOM from 'react-dom';
-import { Toolbar, AppBar, Grid, Button, Container } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles'; 
-import About from './about'; 
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Toolbar, AppBar, Grid, Button, Container } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import About from "./about";
+import Resume from "../resume.pdf";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    toolbar: {
-      alignItems: 'flex-center',
-      paddingTop: theme.spacing(1),
-      paddingBottom: theme.spacing(1),
-    }
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  toolbar: {
+    alignItems: "flex-center",
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+}));
 
 export default function Navbar() {
-    
-    
-        const classes = useStyles();
-        return (
-            <div className={classes.root}>
-                <AppBar color="primary" position="sticky">
-                    <Toolbar className={classes.toolbar}>
-                        {['About','Resumé', 'Research', 'Projects'].map((value) => (
-                            <Button color="inherit" key={value}>{value}</Button>
-                        ))}
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar color="primary" position="sticky">
+        <Toolbar className={classes.toolbar}>
+          {[
+            ["About", ""],
+            ["Resumé", Resume],
+            ["Research", ""],
+            ["Projects", ""],
+          ].map((value) => (
+            <Button color="inherit" href={value[1]} key={value[0]}>
+              {value[0]}
+            </Button>
+          ))}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-
